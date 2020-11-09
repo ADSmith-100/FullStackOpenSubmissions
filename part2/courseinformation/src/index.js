899b9eada6ccadc27c4fe48b227acf6ee671fcb2
Adam Smith <coloradoasmith@gmail.com>
2020-11-09 14:44:39 -0700

    2.1 complete

--- part2/courseinformation/src/index.js
+++ part2/courseinformation/src/index.js
@@ -5,13 +5,13 @@ const Header = ({ course }) => {
   return <h1>{course.name}</h1>;
 };
 
-const Total = ({ course }) => {
-  const sum =
-    course.parts[0].exercises +
-    course.parts[1].exercises +
-    course.parts[2].exercises;
-  return <p>Number of exercises {sum}</p>;
-};
+// const Total = ({ course }) => {
+//   const sum =
+//     course.parts[0].exercises +
+//     course.parts[1].exercises +
+//     course.parts[2].exercises;
+//   return <p>Number of exercises {sum}</p>;
+// };
 
 const Part = (props) => {
   return (
@@ -31,32 +31,39 @@ const Content = ({ course }) => {
   );
 };
 
+const Course = (props) => {
+  return (
+    <>
+      <Header course={props.course} part={props.part} />
+      <Content course={props.course} part={props.part} />
+    </>
+  );
+};
+
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
