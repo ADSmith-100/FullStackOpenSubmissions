a2f55d49bd3ebce58727fc3218018bce7673260c
Adam Smith <coloradoasmith@gmail.com>
2020-11-10 11:24:36 -0700

    2.5 complete

--- part2/courseinformation/src/index.js
+++ part2/courseinformation/src/index.js
@@ -1,46 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import Course from "./Course/";
-
-const Header = ({ course }) => {
-  return <h2>{course.name}</h2>;
-};
-
-const Total = (props) => {
-  let totes = props.course.parts.reduce((a, b) => a + b.exercises, 0);
-
-  return (
-    <strong>
-      <p>Total exercises: {totes} </p>
-    </strong>
-  );
-};
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name}: {props.part.exercises}
-    </p>
-  );
-};
-
-const Content = ({ course }) => {
-  const courseParts = course.parts.map((part, i) => (
-    <Part part={course.parts[i]} key={course.parts[i].id} />
-  ));
-
-  return <div>{courseParts}</div>;
-};
-
-const Course = (props) => {
-  return (
-    <>
-      <Header course={props.course} />
-      <Content course={props.course} part={props.part} />
-      <Total course={props.course} />
-    </>
-  );
-};
+import Course from "./Course/Course";
 
 const App = () => {
   const courses = [
