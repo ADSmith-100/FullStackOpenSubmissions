6ebe8e241c5c06fdbc3644f155d28ca2d24baa32
Adam Smith <coloradoasmith@gmail.com>
2020-11-12 19:50:15 -0700

    button show data 2.13 complete

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -2,6 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+const SelectedCountry = (props) => {
+  return props.filteredData.map((country) => (
+    <div key={country.name}>
+      <h2>{country.name}</h2>
+      <p>capital: {country.capital}</p>
+      <p>population: {country.population}</p>
+      <br></br>
+      <h2>Languages:</h2>
+      {props.filteredData[0].languages.map((lang, i) => (
+        <ul key={i}>
+          <li>{lang.name}</li>
+        </ul>
+      ))}
+      <img src={props.filteredData[0].flag} alt="flag of filtered country" />
+    </div>
+  ));
+};
+
 const App = () => {
   const [countries, setCountries] = useState([]);
   const [searchName, setNewSearchName] = useState("");
@@ -11,9 +29,7 @@ const App = () => {
   };
 
   useEffect(() => {
-    console.log("effect");
     axios.get("https://restcountries.eu/rest/v2/all").then((response) => {
-      console.log("promise fulfilled");
       setCountries(response.data);
     });
   }, []);
@@ -22,34 +38,31 @@ const App = () => {
     country.name.toLowerCase().includes(searchName.toLowerCase())
   );
 
-  const CountryData = () => {
-    if (filteredData.length > 10) {
+  const showSelectedCountry = (event) => {
+    setNewSearchName(event.target.value);
+  };
+
+  const CountryData = (props) => {
+    if (props.filteredData.length > 10) {
       return <p>Too many matches, please specify another filter</p>;
-    } else if (filteredData.length < 10 && filteredData.length > 1) {
-      return filteredData.map((country) => (
+    } else if (
+      props.filteredData.length < 10 &&
+      props.filteredData.length > 1
+    ) {
+      return props.filteredData.map((country) => (
         <ul key={country.name}>
-          <li>{country.name}</li>
+          <li>
+            {country.name}{" "}
+            <button onClick={showSelectedCountry} value={country.name}>
+              show
+            </button>
+          </li>
         </ul>
       ));
     } else {
-      return filteredData.map((country) => (
-        <div key={country.name}>
-          <h2>{country.name}</h2>
-          <p>capital: {country.capital}</p>
-          <p>population: {country.population}</p>
-          <br></br>
-          <h2>Languages:</h2>
-          {filteredData[0].languages.map((lang, i) => (
-            <ul key={i}>
-              <li>{lang.name}</li>
-            </ul>
-          ))}
-          <img src={filteredData[0].flag} alt="flag of filtered country" />
-        </div>
-      ));
+      return <SelectedCountry filteredData={filteredData} />;
     }
   };
-
   return (
     <div>
       <h1>Country Data!</h1>
